d5be484903dda6fbf0a4d4c9ab1932b848756563
Home <home@192.168.1.176>
2024-03-05 11:05:59 +0000

    Initial Commit

---
+++ Weekly-Tasks/S3/Website/Models/SchoolContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Website.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
